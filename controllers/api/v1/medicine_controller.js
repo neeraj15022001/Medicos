979df60da6e44c65e38dcecf45cf3909d48d9e58
neeraj15022001 <gneeraj32595@gmail.com>
2022-02-17 14:32:38 +0530

    medicine controller setup and tested

--- controllers/api/v1/medicine_controller.js
+++ controllers/api/v1/medicine_controller.js
@@ -1,16 +1,56 @@
-module.exports.post = (req, res) => {
-    return res.status(200).json({
-        message: "Medicine Posted"
-    })
+const Medicine = require("../../../models/Medicine")
+module.exports.post = async (req, res) => {
+    const medicine = req.body;
+    let medicineSearched = await Medicine.findOne({name: medicine.name, company: medicine.company});
+    if (medicineSearched) {
+        return res.status(200).json({
+            message: "Medicine Already Exist",
+            medicine: medicineSearched
+        });
+    }
+    try {
+        let medicineCreated = await Medicine.create(req.body);
+        return res.status(200).json({
+            message: "Medicine Posted",
+            medicine: medicineCreated
+        })
+    } catch (e) {
+        return res.status(500).json({
+            message: "Error occured while posting medicine",
+            error: e
+        })
+    }
 }
 
-module.exports.remove = (req, res) => {
-    return res.status(200).json({
-        message: "Medicine Removed"
-    })
+module.exports.remove = async (req, res) => {
+    try {
+        let medicineSearched = await Medicine.findById(req.query.id);
+        if (medicineSearched) {
+            await Medicine.deleteOne({_id: medicineSearched._id});
+            return res.status(200).json({
+                message: "Message Deleted Successfully"
+            })
+        }
+    } catch (e) {
+        return res.status(500).json({
+            message: "Error occured while deleting medicine",
+            error: e
+        })
+    }
+
+
 }
-module.exports.find = (req, res) => {
-    return res.status(200).json({
-        message: "Finding Medicine"
-    })
+module.exports.find = async (req, res) => {
+    try {
+        let medicinesSearched = await Medicine.find({name: {$regex: req.query.name, $options: "gi"}});
+        return res.status(200).json({
+            message: `User Retrieved with search query ${req.query.name}`,
+            medicines: medicinesSearched
+        })
+    } catch (e) {
+        return res.status(500).json({
+            message: "Error occurred while searching medicine",
+            error: e
+        })
+    }
 }
